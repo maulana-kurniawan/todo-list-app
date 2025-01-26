7b4ddc6bca5b2127d4691ff8d7520ac623481f03
maulana-kurniawan <294797997+maulana-kurniawan@users.noreply.github.com>
2025-01-27 05:51:30 +0700

    add user email in navbar

--- resources/views/component/navbar.blade.php
+++ resources/views/component/navbar.blade.php
@@ -1,6 +1,9 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
 		<div class="container-fluid">
-				<div class="navbar-brand">{{ Auth::user()->name }}</div>
+				<div class="d-flex flex-column">
+						<span class="navbar-brand fw-bold mb-0">{{ Auth::user()->name }}</span>
+						<span class="fs-6 text-muted">{{ Auth::user()->email }}</span>
+				</div>
 				<form
 						style="display: inline"
 						action="{{ route('logout') }}"
