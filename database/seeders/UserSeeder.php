5bf6d1e87ccbd0e9e3adacf0b4839a198d898bf8
maulana-kurniawan <294797997+maulana-kurniawan@users.noreply.github.com>
2025-01-27 05:49:58 +0700

    remove user factory, use predefined seeder instead

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,11 +4,16 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
 	public function run()
 	{
-		User::factory(10)->create();
+		User::create([
+			'name' => 'User',
+			'email' => 'user@example.com',
+			'password' => Hash::make('123'),
+		]);
 	}
 }
